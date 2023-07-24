ab289bfed1a59d9da58b7a45c3bbfb80ee290bab
Brian <wamwangimathenge@gmail.com>
2023-07-24 12:43:05 +0300

    Build and call a function
    This commit provides a function that accepts an array and iterates over
    through it.

---
+++ script.js
@@ -0,0 +1,13 @@
+function listArrayItems(arr) {
+    for(i=0; i<arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
+
+console.log("-----------------------")
+
+var models = ["Audi", "BMW", "Mazda", "Mercedes", "Porsche", "Toyota", "Subaru", "Volkswagen"];
+listArrayItems(models);
